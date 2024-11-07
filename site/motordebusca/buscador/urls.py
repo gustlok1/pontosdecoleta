e4f056d134021fa3ac05dede61228df9af2f8f71
Gustavo Henrique Moises Martins <gustavo.martins@orion.inf.br>
2024-11-07 14:23:34 -0300

    Criação de segunda página de consulta e reorganização do index

--- site/motordebusca/buscador/urls.py
+++ site/motordebusca/buscador/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    path(r'david/', views.david, name='david')
+    path(r'david/', views.david, name='david'),
+    path(r'resultados/', views.resultados, name='resultados')
 ]
--- site/motordebusca/buscador/views.py
+++ site/motordebusca/buscador/views.py
@@ -3,9 +3,25 @@ from .models import Pontos
 
 # Create your views here.
 def index(req):
-    pontos = {"pontos": Pontos.objects.all() }  
-    resultados = "São Paulo"
-    return render(req, "buscador/index.html", pontos )
+    #pontos = {"pontos": Pontos.objects.all() }  
+    #resultados = "São Paulo"
+    return render(req, "buscador/index.html")
+
+def resultados(req):
+    pontos = Pontos.objects.all()
+    cidade = req.POST.get('cidade')
+    filtrados = ""
+    if cidade == "": 
+        filtrados = { "pontos":  pontos }
+    else: 
+        filtrados = { "pontos":  [obj for obj in pontos if obj.cidade == cidade] }
+     
+    return render(req, "buscador/resultados.html", filtrados)
+
 def david(req):
+    return render(req, "buscador/david.html")
+
+
+def roberto(req):
     return render(req, "buscador/david.html")
     #return HttpResponse("Boa tarde")
